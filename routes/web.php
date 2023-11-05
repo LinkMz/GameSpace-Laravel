2f8d1646dae1080ec51124af35a48693f7958993
LinkMz <112184666+LinkMz@users.noreply.github.com>
2023-11-05 12:33:54 -0500

    ...

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\GamesControllers;
+
+Route::get('/',[GamesControllers::class,'index'])->name('authors.index');
+Route::post('/authors',[GamesControllers::class,'store'])->name('authors.store');
+Route::delete('/authors/{id}',[GamesControllers::class,'destroy'])->name('authors.destroy');
+Route::put('/authors/{id}',[GamesControllers::class,'update'])->name('authors.update');
+Route::get('/authors_edit/{id}',[GamesControllers::class,'edit'])->name('authors.edit');
+
+Route::get('/', function () {
+    return view('welcome');
+});
